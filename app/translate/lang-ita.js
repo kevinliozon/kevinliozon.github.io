683f4a05b1d434990e2e660929d24ffdf635213f
Kevin Liozon <kevinliozonpro@gmail.com>
2017-04-01 21:22:39 +0100

    test

--- app/translate/lang-ita.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LANG_ITA_NAME = 'ita';
-exports.LANG_ITA_TRANS = {
-    'hello world': 'ciao mondo',
-};
-//# sourceMappingURL=lang-ita.js.map
