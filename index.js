7a028687a802ba034ad3f03853a8f332e9108433
Kevin Liozon <kevinliozonpro@gmail.com>
2020-12-25 21:21:47 +0000

    email integration 2

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ window.addEventListener('load', (event) => {
 window.addEventListener('hashchange', (event) => {
     moduleRouter.navChecker();
 });
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "kevinliozonpro@gmail.com",
+        Password : "F40AE6F5275E51807EAE386673651A49FF36",
+        To : 'kevinliozonpro@gmail.com',
+        From : "kevinliozonperso@gmail.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+      message => alert(message)
+    );
+    }
