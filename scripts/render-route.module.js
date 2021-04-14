1a4e4d430162f98ad32d6c8d0a33a05fe0493b10
Kevin Liozon <kevinliozonpro@gmail.com>
2021-04-14 22:27:06 +0100

    fix variable

--- scripts/render-route.module.js
+++ scripts/render-route.module.js
@@ -233,7 +233,7 @@ var moduleRouter = function () {
         /* Sample function that returns boolean in case the browser is Internet Explorer*/
 
         function isIE() {
-          ua = navigator.userAgent;
+          var ua = navigator.userAgent;
           /* MSIE used to detect old browsers and Trident used to newer ones*/
 
           var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
