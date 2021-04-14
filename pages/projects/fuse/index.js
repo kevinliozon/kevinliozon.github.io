2cdbcfba85cd148dc132ad963bde0b789ba2d104
Kevin Liozon <kevinliozonpro@gmail.com>
2021-04-14 15:40:36 +0100

    conversion to es5

--- pages/projects/fuse/index.js
+++ pages/projects/fuse/index.js
@@ -1,18 +1,24 @@
+"use strict";
+
 /**
  * CONTROLLER FOR:
  * FUSE PROJECT PAGE
  * 
  * This script will replace the previous page's controller thanks to getPageController method in router module
  */
-
-new Promise((resolve, reject) => {
+new Promise(function (resolve, reject) {
   if (document.getElementById('sidenav')) {
-    moduleNav.buildNavProject(document.getElementById('sidenav')) // Build the sidenav
+    moduleNav.buildNavProject(document.getElementById('sidenav')); // Build the sidenav
+
     resolve(); // Detect the links and build their navigation listener
   } else {
     // Will listen to the links across the page in all cases
-    reject('Sidenav element does not exist')
+    reject('Sidenav element does not exist');
   }
-})
-.finally(() => moduleRouter.linksListener('js-link--content'))
-.then(result => moduleRouter.hashListener('js-link--hash'), err => console.error('error:', err))
+})["finally"](function () {
+  return moduleRouter.linksListener('js-link--content');
+}).then(function (result) {
+  return moduleRouter.hashListener('js-link--hash');
+}, function (err) {
+  return console.error('error:', err);
+});
