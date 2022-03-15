a96db8650e4de76452cb996b6bcfd0df88fc544a
Kevin Liozon <kevinliozonpro@gmail.com>
2022-03-15 04:16:10 +0000

    fallback es5

--- components/fallback-list.js
+++ components/fallback-list.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * COMPONENT FOR:
  * FALLBACK LIST OF LINKS
@@ -5,23 +7,23 @@
  * Generates and HTML <ul> node with helper links
  */
 
-let fallbackLinksComponent = document.createElement('ul');
+var fallbackLinksComponent = document.createElement('ul');
 fallbackLinksComponent.classList.add('c-list', 'c-list--links');
 fallbackLinksComponent.innerHTML = '\
 <li class="c-list__i">\
   <a class="c-link js-link"\
-  href="'+pages[0].href+'"\
-  aria-label="'+pages[0].label+'"\
-  data-template="'+pages[0].templatePath+'"\
-  data-name="'+pages[0].name+'"\
+  href="' + pages[0].href + '"\
+  aria-label="' + pages[0].label + '"\
+  data-template="' + pages[0].templatePath + '"\
+  data-name="' + pages[0].name + '"\
   target="_top">Return to home page</a>\
 </li>\
 <li class="c-list__i">\
   <a class="c-link js-link"\
-  href="'+pages[3].href+'"\
-  aria-label="'+pages[3].label+'"\
-  data-template="'+pages[3].templatePath+'"\
-  data-name="'+pages[3].name+'"\
+  href="' + pages[3].href + '"\
+  aria-label="' + pages[3].label + '"\
+  data-template="' + pages[3].templatePath + '"\
+  data-name="' + pages[3].name + '"\
   target="_top">Contact me</a>\
 </li>\
 <li class="c-list__i">\
